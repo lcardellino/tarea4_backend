7b763851208cd7839636e16440f5baf3fa16b95b
Lucas Cardellino <91224219+lcardellino@users.noreply.github.com>
2022-11-25 18:06:05 -0300

    Add files via upload

--- routes/producto.js
+++ routes/producto.js
@@ -1,10 +1,44 @@
 const express = require('express')
-const { verProducto } = require('../controllers/productoController')
 const router = express.Router()
-const {crearProducto, verProductos} = require("../controllers/productoController")
+const {crearProducto, verProductos, editarProducto, vistaUnicaProducto, eliminarProducto} = require("../controllers/productoController")
+const {validarId} = require("../middlewares/validarId")
+const {check} = require("express-validator")
 
 
-router.post('/crear', crearProducto)
+
+//===========> GET <=============//
 router.get('/ver', verProductos)
+router.get('/ver/:id', validarId, vistaUnicaProducto)
 
-module.exports = router
+
+
+//===========> POST <=============//
+router.post('/crear', [
+    check("marca").not().isEmpty().withMessage("marca requerida").isLength({min: 3, max: 15}).withMessage("El campo debe tener mas de 3 letras y menos de 15"),  
+    check("droga").not().isEmpty().withMessage("droga generica requerida"),
+    check("laboratorio").not().isEmpty().withMessage("laboratorio requerido"),
+    check("cantidad").not().isEmpty().withMessage("cantidad requerida"),
+    check("concentracion").not().isEmpty().withMessage("concentracion requerida"),
+    check("prescripcion").not().isEmpty().withMessage("venta bajo prescripcion requerida"),
+], crearProducto);
+
+
+
+//===========> PUT <=============//
+router.put('/editar/:id', validarId, [
+    check("marca").not().isEmpty().withMessage("marca requerida").isLength({min: 3, max: 15}).withMessage("El campo debe tener mas de 3 letras y menos de 15"),  
+    check("droga").not().isEmpty().withMessage("droga generica requerida"),
+    check("laboratorio").not().isEmpty().withMessage("laboratorio requerido"),
+    check("cantidad").not().isEmpty().withMessage("cantidad requerida"),
+    check("concentracion").not().isEmpty().withMessage("concentracion requerida"),
+    check("prescripcion").not().isEmpty().withMessage("venta bajo prescripcion requerida"),], editarProducto)
+
+
+
+//===========> DELETE <=============//
+router.delete('/delete/:id', validarId, eliminarProducto)
+
+
+
+
+module.exports = router;
