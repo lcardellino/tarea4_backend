427ca21c436d45cad5cc37a1c9fbec0f8a3cbd29
Lucas Cardellino <91224219+lcardellino@users.noreply.github.com>
2022-11-25 18:04:42 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const logger = require("morgan")
-
+const cors = require("cors")
 const {conect} = require("./db/db")
 
 const indexRouter = require("./routes/index");
 const productoRouter = require("./routes/producto")
 
-app.use(logger("dev"));
-app.use(express.json());
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(cors())
 
 app.use("/", indexRouter);
 app.use("/drogueria", productoRouter);
